e22fad7bb324269fc448e5395a1e3ae33c536ebe
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-04-04 16:14:02 +0200

    Update canvas.lua
    Added drawRectangle()

--- canvas.lua
+++ canvas.lua
@@ -209,3 +209,71 @@ function drawCircle(x, y, radius, options)
     if border then stroke_circle({x, y}, radius, borderWidth, borderPaint) end
     restore()
 end
+
+function drawRectangle(x, y, width, height, options)
+    --[[
+    Draws a rectangle at a given position.
+
+    Arguments:
+    - x (number): x coordinate of rectangle
+    - y (number): y coordinate of rectangle
+    - width (number): width of rectangle
+    - height (number): height of rectangle
+    - options (table): table of optional arguments with default values
+        - fill (boolean): whether to fill the rectangle, defaults
+            to true
+        - cornerRadius (number): radius of corners, defaults to 0
+        - proportionalRadius (boolean): whether to use a proportional
+            radius, defaults to false
+        - paint (paint): paint color of rectangle, defaults to white
+        - origin (string): origin of rectangle, defaults to "sw"
+        - rotation (number): rotation of rectangle where 0 is no
+            rotation and 1 is a full rotation, defaults to 0.
+        - border (boolean): whether to draw a border, defaults to false
+        - borderWidth (number): width of border, defaults to size/50
+        - borderPaint (paint): paint color of border, defaults
+            to theme.text
+
+    Returns:
+    - None
+    ]]
+    
+    local function getRectangleCoords(x, y, width, height, origin)
+        local origins = {
+            ["n"] = function() return {{x - width/2, y - height}, {x + width/2, y}} end,
+            ["ne"] = function() return {{x - width, y - height}, {x, y}} end,
+            ["e"] = function() return {{x - width, y - height/2}, {x, y + height/2}} end,
+            ["se"] = function() return {{x - width, y}, {x, y + height}} end,
+            ["s"] = function() return {{x - width/2, y}, {x + width/2, y + height}} end,
+            ["sw"] = function() return {{x, y}, {x + width, y + height}} end,
+            ["w"] = function() return {{x, y - height/2}, {x + width, y + height/2}} end,
+            ["nw"] = function() return {{x, y - height}, {x + width, y}} end,
+            ["c"] = function() return {{x - width/2, y - height/2}, {x + width/2, y + height/2}} end,
+        }
+        return origins[origin]()
+    end
+
+
+    local options = options or {}
+    local fill = options.fill
+    if fill == nil then fill = true end
+    local cornerRadius = options.cornerRadius or 0
+    local proportionalRadius = options.proportionalRadius or false
+    local paint = options.paint or color_paint{1, 1, 1, 1}
+    local origin = options.origin or "sw"
+    local rotation = options.rotation or 0
+    local coords = getRectangleCoords(x, y, width, height, origin)
+    local border = options.border or false
+    local borderWidth = options.borderWidth or width/50
+    local borderPaint = options.borderPaint or color_paint{theme.text[1], theme.text[2], theme.text[3], theme.text[4]}
+
+    if proportionalRadius then
+        cornerRadius = cornerRadius * width * 0.01
+    end
+
+    save()
+    rotate(rotation * 2 * math.pi)
+    if fill then fill_rect(coords[1], coords[2], cornerRadius, paint) end
+    if border then stroke_rect(coords[1], coords[2], cornerRadius, borderWidth, borderPaint) end
+    restore()
+end
