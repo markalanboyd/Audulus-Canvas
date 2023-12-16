2885eddf01243056ef78ce6802cff56f1229009c
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-12-16 15:28:04 +0100

    Add shrinkOnPress, border and refactor
    Button can shrink on press now. Border is possible and the border goes inwards. Refactored the tileFn to remove unnecessary args. Now also possible to have a height separat from width.

--- src/draw/create_button.lua
+++ src/draw/create_button.lua
@@ -11,59 +11,112 @@ local grid = {
     x1y4, x2y4, x3y4, x4y4
 }
 
-local function btn(x, y, size, options)
-	local o = options or {}
-	local id = o.id or 0
-	local c = o.color or { 1, 1, 1, 1 }
-
-    local pad = o.pad or 0
-	local pPad = o.pPad or false
-    if pPad then
-        if pad ~= 0 then
-            error(string.format("Remove pad argument or set pPad = false"))
+local function btn(x, y, width, options)
+    -- Error Handling
+    local function checkMutuallyExclusiveArgs(arg1Name, arg1, arg2Name, arg2)
+        local e = "MutuallyExclusiveArgError: '%s' and '%s' cannot be used" ..
+                  "together. Remove one or the other."
+        if arg1 and arg2 then
+            error(string.format(e, arg1Name, arg2Name))
         end
-        local pPadPercent = o.pPadPercent or 5
-        pad = size * ( pPadPercent / 100 )
     end
 
-	local radius = o.radius or 0
+    -- Optional Keyword Arguments
+    local o = options or {}
+    local height = o.height or width
+    local id = o.id or 0
+    local c = o.color or { 1, 1, 1, 1 }
+
+    -- Corner Radius
+    local r = o.radius or nil
     local pRadius = o.pRadius or false
     if pRadius then
-        if radius ~= 0 then
-            error(string.format("Remove radius argument or set pRadius = false"))
-        end
-        local pRadiusPercent = o.pRadiusPercent or 10
-        radius = size / pRadiusPercent
+        checkMutuallyExclusiveArgs("radius", r, "pRadius", pRadius)
+        local pRadiusPercent = math.min( o.pRadiusPercent or 10, 50 )
+        r = width * ( pRadiusPercent / 100)
     else
-        if radius > size then
-            error(string.format("Radius cannot exceed size." ..
+        if r ~= nil and r > width then
+            error(string.format("ValueError: Radius cannot exceed size." ..
                                 "Max: %d, " ..
                                 "Provided: %d",
-                                size, radius))
+                                width, r))
         end
     end
+    local radius = r or 0
 
+    -- Border
+    local border = o.border or false
+    local borderWidth = o.borderWidth or 1
+    local borderColor = o.borderColor or
+                        { c[1] * 0.75, c[2] * 0.75, c[3] * 0.75, c[4] }
 
+    -- Initialize coordinates
+    local x1 = x
+    local y1 = y
+    local x2 = x + width
+    local y2 = y + height
 
-	local x1 = x + pad
-	local y1 = y + pad
-    local x2 = x + size - pad
-    local y2 = y + size - pad
+    -- Touch detector
+    local btnTouched = tX >= x1 and tX <= x2 and tY >= y1 and tY <= y2 and t > 0
+
+    -- Shrink on press
+    local shrinkOnPress = o.shrinkOnPress or false
+    if shrinkOnPress and btnTouched then
+        local shrinkPercent = o.shrinkPercent or 90
+        local sP = shrinkPercent / 100
+        local dX = (x2 - x1) * (1 - sP) / 2
+        local dY = (y2 - y1) * (1 - sP) / 2
+        x1 = x1 + dX
+        y1 = y1 + dY
+        x2 = x2 - dX
+        y2 = y2 - dY
+    end
+
+    -- Padding
+    local p = o.pad or nil
+    local pPad = o.pPad or false
+    if pPad then
+        checkMutuallyExclusiveArgs("pad", p, "pPad", pPad)
+        local pPadPercent = o.pPadPercent or 5
+        p = width * ( pPadPercent / 100 )
+    end
+    local pad = p or 0
+    x1 = x1 + pad
+    y1 = y1 + pad
+    x2 = x2 - pad
+    y2 = y2 - pad
+
+    -- Pack coordinates
     local pA = { x1, y1 }
     local pB = { x2, y2 }
 
-    local function tIsInside()
-        return tX >= x1 and tX <= x2 and tY >= y1 and tY <= y2 and t > 0
-    end
-
+    -- Draw
     local function drawButton()
-	    local br = tIsInside() and 1 or 0
-		local st = grid[id] * 0.5
-	    local f = math.max(0, math.min(br + st, 1))
-	    local paint = color_paint(
+        local br = btnTouched and 1 or 0
+        local st = grid[id] * 0.5
+        local f = math.max(0, math.min(br + st, 1))
+        local paint = color_paint(
             { c[1] * f , c[2] * f, c[3] * f, c[4] }
         )
-    	fill_rect(pA, pB, radius, paint)
+        if not border then
+            fill_rect(pA, pB, radius, paint)
+        else
+            -- Border
+            fill_rect(pA, pB, radius, color_paint(borderColor))
+
+            -- Adjust the radius as border gets thicker
+            local innerWidth = x2 - x1 - borderWidth
+            local innerHeight = y2 - y1 - borderWidth
+            local scaleFactor = math.min(innerWidth  / (x2 - x1),
+                                         innerHeight / (y2 - y1))
+            local adjustedRadius = radius * scaleFactor
+
+            -- Inner rectangle
+            local hBW = borderWidth / 2
+            local pAB = { x1 + hBW, y1 + hBW }
+            local pBB = { x2 - hBW, y2 - hBW }
+            fill_rect(pAB, pBB, adjustedRadius, paint)
+        end
     end
 
     drawButton()
@@ -71,20 +124,12 @@ end
 
 
 local function tileFn(func, r, c)
-    return function(x, y, s, o)
-        local options = {
-            color = o.color,
-            pad = o.pad,
-            pPad = o.pPad,
-            pPadPercent = o.pPadPercent,
-            radius = o.radius,
-            pRadius = o.pRadius,
-            pRadiusPercent = o.pRadiusPercent,
-        }
+    return function(x, y, w, o)
+        local h = o.height or w
         for i = 0, c - 1 do
             for j = 0, r - 1 do
-                options.id = i + 1 + j * c
-                func(x + i * s, y + j * s, s, options)
+                o.id = i + 1 + j * c
+                func(x + i * w, y + j * h, w, o)
             end
         end
     end
@@ -93,8 +138,12 @@ end
 
 local tileBtn = tileFn(btn, 4, 4)
 tileBtn(0, 0, 50, {
-    pPad=true,
-    color=theme.azureHighlight,
-    pRadius=true,
+    color = theme.azureHighlight,
+    pPad = true,
+    pRadius = true,
+    border = true,
+    borderWidth = 3,
+    shrinkOnPress = true,
     }
 )
+
