fe88c40537ec37806c1f1a7ed46e30f89d5164ac
Mark Boyd <markalanboyd@gmail.com>
2023-12-18 12:50:54 +0100

    Update print (#32)
    * Update print.lua
    
    - Add ability to print multiple variables separated by commas
    
    * Update print.lua
    
    - Added ability to print tables with or without keys
    
    ---------
    
    Co-authored-by: Mark Boyd <10804241+markalanboyd@users.noreply.github.com>

--- src/debug/print.lua
+++ src/debug/print.lua
@@ -1,31 +1,63 @@
--- Insert this function definition at the beginning of your script
-
 local function createPrintLogger()
 	local queue = {}
 
-	local function addToPrintQueue(s)
-		table.insert(queue, s)
+	local function addToQueue(...)
+		local function hasNonIntegerKeys(t)
+			for k, _ in pairs(t) do
+				if type(k) ~= "number" or k ~= math.floor(k) or k < 1 then
+					return true
+				end
+			end
+			return false
+		end
+
+		local function tableToString(t)
+			local s = "{ "
+			if hasNonIntegerKeys(t) then
+				for k, v in pairs(t) do
+					s = s .. "[" .. k .. "] = " .. v .. ", "
+				end
+			else
+				for _, v in pairs(t) do
+					s = s .. v .. ", "
+				end
+			end
+			s = s:sub(1, -3) .. " }"
+			return s
+		end
+
+		local args = { ... }
+		local statement = ""
+
+		for i, arg in ipairs(args) do
+			if type(arg) == "table" then
+				arg = tableToString(arg)
+			end
+			statement = statement .. tostring(arg) .. ", "
+		end
+
+		statement = statement:sub(1, -3)
+		table.insert(queue, statement)
 	end
 
 	local function printQueue()
-		translate { 0 , -30 }
+		translate { 0, -30 }
 		text("Print Queue Output", theme.text)
-        translate { 0, -4}
+		translate { 0, -4 }
 		text("_________________", theme.text)
-		translate { 0, -20}
+		translate { 0, -20 }
+
 		for i, s in ipairs(queue) do
 			text(i .. ": " .. s, theme.text)
-			translate { 0 , -14 }
+			translate { 0, -14 }
 		end
 	end
 
-	return addToPrintQueue, printQueue
+	return addToQueue, printQueue
 end
 
 local print, printAll = createPrintLogger()
 
--- Your code goes here
--- Use `print()` as needed
+-- INSERT SCRIPT HERE --
 
--- Call `printAll()` in the last line in your script
 printAll()
