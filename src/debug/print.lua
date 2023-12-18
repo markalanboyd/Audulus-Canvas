3bdb937b1b478901e71e6078bd762c230f95af6e
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-12-18 12:48:18 +0100

    Update print.lua
    - Added ability to print tables with or without keys

--- src/debug/print.lua
+++ src/debug/print.lua
@@ -1,35 +1,59 @@
 local function createPrintLogger()
-    local queue = {}
+	local queue = {}
 
-    local function addToQueue(...)
-    	local args = {...}
-    	local statement = ""
+	local function addToQueue(...)
+		local function hasNonIntegerKeys(t)
+			for k, _ in pairs(t) do
+				if type(k) ~= "number" or k ~= math.floor(k) or k < 1 then
+					return true
+				end
+			end
+			return false
+		end
 
-    	for i, arg in ipairs(args) do
-    		if i == 1 then
-	    		statement = statement .. arg
-	    	else
-	    		statement = statement .. ", " .. arg
-	    	end
-    	end
+		local function tableToString(t)
+			local s = "{ "
+			if hasNonIntegerKeys(t) then
+				for k, v in pairs(t) do
+					s = s .. "[" .. k .. "] = " .. v .. ", "
+				end
+			else
+				for _, v in pairs(t) do
+					s = s .. v .. ", "
+				end
+			end
+			s = s:sub(1, -3) .. " }"
+			return s
+		end
 
-    	table.insert(queue, statement)
-    end
+		local args = { ... }
+		local statement = ""
 
-    local function printQueue()
-        translate { 0 , -30 }
-        text("Print Queue Output", theme.text)
-        translate { 0, -4}
-        text("_________________", theme.text)
-        translate { 0, -20}
+		for i, arg in ipairs(args) do
+			if type(arg) == "table" then
+				arg = tableToString(arg)
+			end
+			statement = statement .. tostring(arg) .. ", "
+		end
 
-        for i, s in ipairs(queue) do
-            text(i .. ": " .. s, theme.text)
-            translate { 0 , -14 }
-        end
-    end
+		statement = statement:sub(1, -3)
+		table.insert(queue, statement)
+	end
 
-    return addToQueue, printQueue
+	local function printQueue()
+		translate { 0, -30 }
+		text("Print Queue Output", theme.text)
+		translate { 0, -4 }
+		text("_________________", theme.text)
+		translate { 0, -20 }
+
+		for i, s in ipairs(queue) do
+			text(i .. ": " .. s, theme.text)
+			translate { 0, -14 }
+		end
+	end
+
+	return addToQueue, printQueue
 end
 
 local print, printAll = createPrintLogger()
