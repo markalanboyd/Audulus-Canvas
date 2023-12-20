515ebca42b700b765acdc0fefa83fbfa9ac472fd
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-12-20 06:37:19 +0100

    Update isValidHexCode.lua
    - Add documentation
    - Refactor to use s instead of hex for variable name

--- src/color/validate/isValidHexCode.lua
+++ src/color/validate/isValidHexCode.lua
@@ -1,7 +1,15 @@
-function isValidHexCode(hex)
-    hex = hex:gsub("#", "")
-    local invalidChars = string.match(hex, "[^0-9a-fA-F]+")
+--- Checks if a CSS hex code is formatted properly.
+-- Uses character and length matching to assert that the hex code is
+-- formatted correctly. Works for rgb, rgba, rrggbb, and rrggbbaa hex
+-- codes. Outputs true if correct and false if malformed.
+-- @tparam string s A hex code formatted as #rgb, #rgba, #rrggbb, or
+-- #rrggbbaa with or without the leading # symbol
+-- @treturn bool boolean True if correctly formatted, false if not.
+-- @see hexCodeToColor Uses this validator
+local function isValidHexCode(s)
+    s = s:gsub("#", "")
+    local invalidChars = string.match(s, "[^0-9a-fA-F]+")
     local hasValidChars = invalidChars == nil
-    local isValidLen = #hex == 3 or #hex == 4 or #hex == 6 or #hex == 8
+    local isValidLen = #s == 3 or #s == 4 or #s == 6 or #s == 8
     return hasValidChars and isValidLen
 end
