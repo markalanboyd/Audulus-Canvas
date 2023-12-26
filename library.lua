eb53511b02f1bcf47ec3ff73e59c723750c8c081
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-12-26 08:33:53 +0100

    Correction to truncate
    I realized the string format method was actually rounding numbers. This sets aside the round as its own function while I corrected the truncate one.

--- library.lua
+++ library.lua
@@ -116,9 +116,13 @@ end
 
 MathUtils = {}
 
-function MathUtils.truncate(n, places)
+function MathUtils.round(n, places)
     local s = "%." .. places .. "f"
     return tonumber(string.format(s, n))
+end
+
+function MathUtils.truncate(n, places)
+    return math.floor(n * 10 ^ places) / 10 ^ places
 end
 
 function MathUtils.is_positive_int(n)
