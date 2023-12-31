bb5e00ef382a1d11a7402040d7829148dc2d5b5a
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-12-31 15:10:07 +0100

    Update Point.lua
    Expanded index function to deal with defaults

--- src/draw/Point.lua
+++ src/draw/Point.lua
@@ -1,30 +1,86 @@
 Point = {}
-Point.__index = Point
 
-function Point.new(vec2, color, size)
+Point.id = 1
+
+Point.attrs = {
+    color = theme.text
+}
+
+Point.styles = {
+    normal = {
+        radius = 2,
+    },
+    stroke = {
+        radius = 3,
+        stroke_width = 1,
+    },
+    char = {
+        char = "o",
+
+    },
+}
+
+Point.__index = function(instance, key)
+    local value = rawget(instance, key)
+    if value ~= nil then
+        return value
+    else
+        local style = rawget(instance, "style") or "normal"
+        local style_val = Point.styles[style][key]
+        if style_val ~= nil then
+            return style_val
+        end
+
+        local attr = Point.attrs[key]
+        if attr ~= nil then
+            return attr
+        end
+    end
+
+    return Point[key]
+end
+
+function Point.new(vec2, options)
     local self = setmetatable({}, Point)
-    self.vec2 = vec2 or { x = 0, y = 0 }
-    self.color = color or { 1, 1, 1, 1 }
-    self.size = size or 2
+    self.type = "Point"
+    self.element_id = Element.id
+    Element.id = Element.id + 1
+    self.class_id = Point.id
+    Point.id = Point.id + 1
+
+    self.vec2 = vec2 or Vec2.new(0, 0)
+    self.o = options or {}
+
+    self.style = self.o.style or "normal"
+    self.color = self.color.size or Point.attrs.color
+
+    for key, value in pairs(self.o) do
+        self[key] = value
+    end
+
     return self
 end
 
-function Point.draw_all(...)
-    local points = Utils.process_args(Point, ...)
+function Point:draw_normal()
+    fill_circle(
+        { self.vec2.x, self.vec2.y },
+        self.radius,
+        color_paint(self.color)
+    )
+end
 
-    for _, point in ipairs(points) do
-        if getmetatable(point) == Point then
-            point:draw()
-        else
-            error("Invalid argument to Point.draw_all:" ..
-                "Expected a Point instance or a table of Point instances")
-        end
-    end
+function Point:draw_stroke()
+    stroke_circle(
+        { self.vec2.x, self.vec2.y },
+        self.radius,
+        self.stroke_width,
+        color_paint(self.color))
 end
 
 function Point:draw()
-    fill_circle({ self.vec2.x, self.vec2.y },
-        self.size,
-        color_paint(self.color)
-    )
+    if self.style == "normal" then
+        self:draw_normal()
+    elseif self.style == "stroke" then
+        self:draw_stroke()
+    end
 end
