c735d01c9500badf69d42585c6fe51411a3279e4
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-01-16 10:07:10 +0100

    Update Color.lua
    Add many new methods for deriving colors and includes a debug printing method that allows you to see color schemes

--- src/color/Color.lua
+++ src/color/Color.lua
@@ -114,6 +114,36 @@ function Color.hsla_to_rgba(hsla_table)
     return { r, g, b, a }
 end
 
+function Color.print_swatches(colors)
+    local size = 10
+    for i, color in ipairs(colors) do
+        local x = -i * size
+        fill_rect({ x, 0 }, { x + size, size }, 0, color:to_paint())
+    end
+end
+
+function Color:analogous(offset_degrees)
+    local offset = offset_degrees or 30
+    local hsla = Color.rgba_to_hsla(self:table())
+
+    local hsla1 = { (hsla[1] - offset) % 360, hsla[2], hsla[3], hsla[4] }
+    local rgba1 = Color.hsla_to_rgba(hsla1)
+    local color1 = Color.new(rgba1)
+
+    local hsla2 = { (hsla[1] + offset) % 360, hsla[2], hsla[3], hsla[4] }
+    local rgba2 = Color.hsla_to_rgba(hsla2)
+    local color2 = Color.new(rgba2)
+
+    return color1, color2
+end
+
+function Color:brightness(brightness)
+    local r = self.r * brightness
+    local g = self.g * brightness
+    local b = self.b * brightness
+    return Color.new({ r, g, b, self.a })
+end
+
 function Color:Brightness(brightness)
     self.r = self.r * brightness
     self.g = self.g * brightness
@@ -125,17 +155,40 @@ function Color:clone()
     return Color.new({ self.r, self.g, self.b, self.a })
 end
 
-function Color:fade(color2, t)
-    local new_color = {}
-    new_color[1] = self.r + (color2.r - self.r) * t
-    new_color[2] = self.g + (color2.g - self.g) * t
-    new_color[3] = self.b + (color2.b - self.b) * t
-    new_color[4] = self.a + (color2.a - self.a) * t
-    return new_color
+function Color:complementary()
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[1] = (hsla[1] + 180) % 360
+    local rgba = Color.hsla_to_rgba(hsla)
+    return Color.new(rgba)
 end
 
-function Color:Hue(normalized_hue)
-    local h = normalized_hue * 360
+function Color:Complimentary()
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[1] = (hsla[1] + 180) % 360
+    local rgba = Color.hsla_to_rgba(hsla)
+    self:Set(rgba)
+    return self
+end
+
+function Color:lerp(color2, t)
+    local interpolated_color = {}
+    interpolated_color[1] = self.r + (color2.r - self.r) * t
+    interpolated_color[2] = self.g + (color2.g - self.g) * t
+    interpolated_color[3] = self.b + (color2.b - self.b) * t
+    interpolated_color[4] = self.a + (color2.a - self.a) * t
+    return Color.new(interpolated_color)
+end
+
+function Color:hue(hue_normalized)
+    local h = hue_normalized * 360
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[1] = h
+    local rgba = Color.hsla_to_rgba(hsla)
+    return Color.new(rgba)
+end
+
+function Color:Hue(hue_normalized)
+    local h = hue_normalized * 360
     local hsla = Color.rgba_to_hsla(self:table())
     hsla[1] = h
     local rgba = Color.hsla_to_rgba(hsla)
@@ -143,11 +196,25 @@ function Color:Hue(normalized_hue)
     return self
 end
 
+function Color:invert()
+    local r = 1 - self.r
+    local g = 1 - self.g
+    local b = 1 - self.b
+    return Color.new({ r, g, b, self.a })
+end
+
 function Color:Invert()
     self.r = 1 - self.r
     self.g = 1 - self.g
     self.b = 1 - self.b
     return self
+end
+
+function Color:lightness(lightness)
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[3] = lightness
+    local rgba = Color.hsla_to_rgba(hsla)
+    return Color.new(rgba)
 end
 
 function Color:Lightness(lightness)
@@ -158,9 +225,28 @@ function Color:Lightness(lightness)
     return self
 end
 
+function Color:opacity(opacity)
+    local a = self.a * opacity
+    return Color.new({ self.r, self.g, self.b, a })
+end
+
 function Color:Opacity(opacity)
     self.a = self.a * opacity
     return self
+end
+
+function Color:rotate(degrees)
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[1] = degrees
+    local rgba = Color.hsla_to_rgba(hsla)
+    return Color.new(rgba)
+end
+
+function Color:saturation(saturation)
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[2] = saturation
+    local rgba = Color.hsla_to_rgba(hsla)
+    return Color.new(rgba)
 end
 
 function Color:Saturation(saturation)
@@ -179,6 +265,76 @@ function Color:Set(color_table)
     return self
 end
 
+function Color:split_complementary(offset_degrees)
+    local offset = offset_degrees or 30
+    local complementary = self:complementary()
+    return complementary:analogous(offset)
+end
+
+function Color:square()
+    local left, right = self:analogous(90)
+    return right, self:complementary(), left
+end
+
 function Color:table()
     return { self.r, self.g, self.b, self.a }
+end
+
+function Color:to_paint()
+    return color_paint(self:table())
+end
+
+function Color:triadic()
+    return self:analogous(120)
+end
+
+function Color:tetradic()
+    local analogous = self:rotate(-60)
+    return analogous:complementary(), self:complementary(), analogous
+end
+
+function Color:print_schemes(origin)
+    origin = origin or { 0, 0 }
+
+    save()
+    translate(origin)
+
+    local vec2 = Vec2.new(5, 2.5)
+
+    Text.new("Complementary", { size = 8, vec2 = vec2 }):draw()
+    local complementary = { self, self:complementary() }
+    Color.print_swatches(complementary)
+
+    translate { 0, -12 }
+
+    Text.new("Analogous", { size = 8, vec2 = vec2 }):draw()
+    local analogous = { self, self:analogous() }
+    Color.print_swatches(analogous)
+
+    translate { 0, -12 }
+
+    Text.new("Split Complementary", { size = 8, vec2 = vec2 }):draw()
+    local split_complementary = { self, self:split_complementary() }
+    Color.print_swatches(split_complementary)
+
+    translate { 0, -12 }
+
+    Text.new("Triadic", { size = 8, vec2 = vec2 }):draw()
+    local triadic = { self, self:triadic() }
+    Color.print_swatches(triadic)
+
+    translate { 0, -12 }
+
+    Text.new("Tetradic", { size = 8, vec2 = vec2 }):draw()
+    local tetradic = { self, self:tetradic() }
+    Color.print_swatches(tetradic)
+
+    translate { 0, -12 }
+
+    Text.new("Square", { size = 8, vec2 = vec2 }):draw()
+    local square = { self, self:square() }
+    Color.print_swatches(square)
+
+
+    restore()
 end
