e07a6816b60781654510fb7f629214dfb338a1ac
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-01-16 10:07:13 +0100

    Update library.lua

--- library.lua
+++ library.lua
@@ -1,5 +1,42 @@
 -- SCROLL TO BOTTOM ----------------------------------------------------
 
+Gradient = {}
+Gradient.__index = Gradient
+
+Gradient.id = 1
+
+function Gradient.new(vec2a, vec2b, color1, color2)
+    local self = setmetatable({}, Gradient)
+    self.element_id = Element.id
+    Element.id = Element.id + 1
+    self.class_id = Gradient.id
+    Gradient.id = Gradient.id + 1
+
+    self.vec2a = vec2a or Vec2.new()
+    self.vec2b = vec2b or Vec2.new()
+    self.color1 = color1 or Color.new()
+    self.color2 = color2 or Color.new()
+    return self
+end
+
+function Gradient:to_paint()
+    return linear_gradient(
+        self.vec2a:to_xy_pair(),
+        self.vec2b:to_xy_pair(),
+        self.color1:table(),
+        self.color2:table()
+    )
+end
+Paint = {}
+Paint.__index = Paint
+
+function Paint.create(color, gradient)
+    if gradient ~= nil then
+        return gradient:to_paint()
+    else
+        return color:to_paint()
+    end
+end
 -- TODO Add gradient
 
 Color = {}
@@ -116,6 +153,36 @@ function Color.hsla_to_rgba(hsla_table)
     return { r, g, b, a }
 end
 
+function Color.print_swatches(colors)
+    local size = 10
+    for i, color in ipairs(colors) do
+        local x = -i * size
+        fill_rect({ x, 0 }, { x + size, size }, 0, color:to_paint())
+    end
+end
+
+function Color:analogous(offset_degrees)
+    local offset = offset_degrees or 30
+    local hsla = Color.rgba_to_hsla(self:table())
+
+    local hsla1 = { (hsla[1] - offset) % 360, hsla[2], hsla[3], hsla[4] }
+    local rgba1 = Color.hsla_to_rgba(hsla1)
+    local color1 = Color.new(rgba1)
+
+    local hsla2 = { (hsla[1] + offset) % 360, hsla[2], hsla[3], hsla[4] }
+    local rgba2 = Color.hsla_to_rgba(hsla2)
+    local color2 = Color.new(rgba2)
+
+    return color1, color2
+end
+
+function Color:brightness(brightness)
+    local r = self.r * brightness
+    local g = self.g * brightness
+    local b = self.b * brightness
+    return Color.new({ r, g, b, self.a })
+end
+
 function Color:Brightness(brightness)
     self.r = self.r * brightness
     self.g = self.g * brightness
@@ -127,17 +194,40 @@ function Color:clone()
     return Color.new({ self.r, self.g, self.b, self.a })
 end
 
-function Color:fade(color2, t)
-    local new_color = {}
-    new_color[1] = self.r + (color2.r - self.r) * t
-    new_color[2] = self.g + (color2.g - self.g) * t
-    new_color[3] = self.b + (color2.b - self.b) * t
-    new_color[4] = self.a + (color2.a - self.a) * t
-    return new_color
+function Color:complementary()
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[1] = (hsla[1] + 180) % 360
+    local rgba = Color.hsla_to_rgba(hsla)
+    return Color.new(rgba)
 end
 
-function Color:Hue(normalized_hue)
-    local h = normalized_hue * 360
+function Color:Complimentary()
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[1] = (hsla[1] + 180) % 360
+    local rgba = Color.hsla_to_rgba(hsla)
+    self:Set(rgba)
+    return self
+end
+
+function Color:lerp(color2, t)
+    local interpolated_color = {}
+    interpolated_color[1] = self.r + (color2.r - self.r) * t
+    interpolated_color[2] = self.g + (color2.g - self.g) * t
+    interpolated_color[3] = self.b + (color2.b - self.b) * t
+    interpolated_color[4] = self.a + (color2.a - self.a) * t
+    return Color.new(interpolated_color)
+end
+
+function Color:hue(hue_normalized)
+    local h = hue_normalized * 360
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[1] = h
+    local rgba = Color.hsla_to_rgba(hsla)
+    return Color.new(rgba)
+end
+
+function Color:Hue(hue_normalized)
+    local h = hue_normalized * 360
     local hsla = Color.rgba_to_hsla(self:table())
     hsla[1] = h
     local rgba = Color.hsla_to_rgba(hsla)
@@ -145,11 +235,25 @@ function Color:Hue(normalized_hue)
     return self
 end
 
+function Color:invert()
+    local r = 1 - self.r
+    local g = 1 - self.g
+    local b = 1 - self.b
+    return Color.new({ r, g, b, self.a })
+end
+
 function Color:Invert()
     self.r = 1 - self.r
     self.g = 1 - self.g
     self.b = 1 - self.b
     return self
+end
+
+function Color:lightness(lightness)
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[3] = lightness
+    local rgba = Color.hsla_to_rgba(hsla)
+    return Color.new(rgba)
 end
 
 function Color:Lightness(lightness)
@@ -160,9 +264,28 @@ function Color:Lightness(lightness)
     return self
 end
 
+function Color:opacity(opacity)
+    local a = self.a * opacity
+    return Color.new({ self.r, self.g, self.b, a })
+end
+
 function Color:Opacity(opacity)
     self.a = self.a * opacity
     return self
+end
+
+function Color:rotate(degrees)
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[1] = degrees
+    local rgba = Color.hsla_to_rgba(hsla)
+    return Color.new(rgba)
+end
+
+function Color:saturation(saturation)
+    local hsla = Color.rgba_to_hsla(self:table())
+    hsla[2] = saturation
+    local rgba = Color.hsla_to_rgba(hsla)
+    return Color.new(rgba)
 end
 
 function Color:Saturation(saturation)
@@ -181,12 +304,84 @@ function Color:Set(color_table)
     return self
 end
 
+function Color:split_complementary(offset_degrees)
+    local offset = offset_degrees or 30
+    local complementary = self:complementary()
+    return complementary:analogous(offset)
+end
+
+function Color:square()
+    local left, right = self:analogous(90)
+    return right, self:complementary(), left
+end
+
 function Color:table()
     return { self.r, self.g, self.b, self.a }
+end
+
+function Color:to_paint()
+    return color_paint(self:table())
+end
+
+function Color:triadic()
+    return self:analogous(120)
+end
+
+function Color:tetradic()
+    local analogous = self:rotate(-60)
+    return analogous:complementary(), self:complementary(), analogous
+end
+
+function Color:print_schemes(origin)
+    origin = origin or { 0, 0 }
+
+    save()
+    translate(origin)
+
+    local vec2 = Vec2.new(5, 2.5)
+
+    Text.new("Complementary", { size = 8, vec2 = vec2 }):draw()
+    local complementary = { self, self:complementary() }
+    Color.print_swatches(complementary)
+
+    translate { 0, -12 }
+
+    Text.new("Analogous", { size = 8, vec2 = vec2 }):draw()
+    local analogous = { self, self:analogous() }
+    Color.print_swatches(analogous)
+
+    translate { 0, -12 }
+
+    Text.new("Split Complementary", { size = 8, vec2 = vec2 }):draw()
+    local split_complementary = { self, self:split_complementary() }
+    Color.print_swatches(split_complementary)
+
+    translate { 0, -12 }
+
+    Text.new("Triadic", { size = 8, vec2 = vec2 }):draw()
+    local triadic = { self, self:triadic() }
+    Color.print_swatches(triadic)
+
+    translate { 0, -12 }
+
+    Text.new("Tetradic", { size = 8, vec2 = vec2 }):draw()
+    local tetradic = { self, self:tetradic() }
+    Color.print_swatches(tetradic)
+
+    translate { 0, -12 }
+
+    Text.new("Square", { size = 8, vec2 = vec2 }):draw()
+    local square = { self, self:square() }
+    Color.print_swatches(square)
+
+
+    restore()
 end
 ColorTables = {}
 
 ColorTables.htmlColors = { indianred = { 205, 92, 92, 1 }, lightcoral = { 240, 128, 128, 1 }, salmon = { 250, 128, 114, 1 }, darksalmon = { 233, 150, 122, 1 }, lightsalmon = { 255, 160, 122, 1 }, crimson = { 220, 20, 60, 1 }, red = { 255, 0, 0, 1 }, firebrick = { 178, 34, 34, 1 }, darkred = { 139, 0, 0, 1 }, pink = { 255, 192, 203, 1 }, lightpink = { 255, 182, 193, 1 }, hotpink = { 255, 105, 180, 1 }, deeppink = { 255, 20, 147, 1 }, mediumvioletred = { 199, 21, 133, 1 }, palevioletred = { 219, 112, 147, 1 }, coral = { 255, 127, 80, 1 }, tomato = { 255, 99, 71, 1 }, orangered = { 255, 69, 0, 1 }, darkorange = { 255, 140, 0, 1 }, orange = { 255, 165, 0, 1 }, gold = { 255, 215, 0, 1 }, yellow = { 255, 255, 0, 1 }, lightyellow = { 255, 255, 224, 1 }, lemonchiffon = { 255, 250, 205, 1 }, lightgoldenrodyellow = { 250, 250, 210, 1 }, papayawhip = { 255, 239, 213, 1 }, moccasin = { 255, 228, 181, 1 }, peachpuff = { 255, 218, 185, 1 }, palegoldenrod = { 238, 232, 170, 1 }, khaki = { 240, 230, 140, 1 }, darkkhaki = { 189, 183, 107, 1 }, lavender = { 230, 230, 250, 1 }, thistle = { 216, 191, 216, 1 }, plum = { 221, 160, 221, 1 }, violet = { 238, 130, 238, 1 }, orchid = { 218, 112, 214, 1 }, fuchsia = { 255, 0, 255, 1 }, magenta = { 255, 0, 255, 1 }, mediumorchid = { 186, 85, 211, 1 }, mediumpurple = { 147, 112, 219, 1 }, rebeccapurple = { 102, 51, 153, 1 }, blueviolet = { 138, 43, 226, 1 }, darkviolet = { 148, 0, 211, 1 }, darkorchid = { 153, 50, 204, 1 }, darkmagenta = { 139, 0, 139, 1 }, purple = { 128, 0, 128, 1 }, indigo = { 75, 0, 130, 1 }, slateblue = { 106, 90, 205, 1 }, darkslateblue = { 72, 61, 139, 1 }, mediumslateblue = { 123, 104, 238, 1 }, greenyellow = { 173, 255, 47, 1 }, chartreuse = { 127, 255, 0, 1 }, lawngreen = { 124, 252, 0, 1 }, lime = { 0, 255, 0, 1 }, limegreen = { 50, 205, 50, 1 }, palegreen = { 152, 251, 152, 1 }, lightgreen = { 144, 238, 144, 1 }, mediumspringgreen = { 0, 250, 154, 1 }, springgreen = { 0, 255, 127, 1 }, mediumseagreen = { 60, 179, 113, 1 }, seagreen = { 46, 139, 87, 1 }, forestgreen = { 34, 139, 34, 1 }, green = { 0, 128, 0, 1 }, darkgreen = { 0, 100, 0, 1 }, yellowgreen = { 154, 205, 50, 1 }, olivedrab = { 107, 142, 35, 1 }, olive = { 128, 128, 0, 1 }, darkolivegreen = { 85, 107, 47, 1 }, mediumaquamarine = { 102, 205, 170, 1 }, darkseagreen = { 143, 188, 139, 1 }, lightseagreen = { 32, 178, 170, 1 }, darkcyan = { 0, 139, 139, 1 }, teal = { 0, 128, 128, 1 }, aqua = { 0, 255, 255, 1 }, cyan = { 0, 255, 255, 1 }, lightcyan = { 224, 255, 255, 1 }, paleturquoise = { 175, 238, 238, 1 }, aquamarine = { 127, 255, 212, 1 }, turquoise = { 64, 224, 208, 1 }, mediumturquoise = { 72, 209, 204, 1 }, darkturquoise = { 0, 206, 209, 1 }, cadetblue = { 95, 158, 160, 1 }, steelblue = { 70, 130, 180, 1 }, lightsteelblue = { 176, 196, 222, 1 }, powderblue = { 176, 224, 230, 1 }, lightblue = { 173, 216, 230, 1 }, skyblue = { 135, 206, 235, 1 }, lightskyblue = { 135, 206, 250, 1 }, deepskyblue = { 0, 191, 255, 1 }, dodgerblue = { 30, 144, 255, 1 }, cornflowerblue = { 100, 149, 237, 1 }, royalblue = { 65, 105, 225, 1 }, blue = { 0, 0, 255, 1 }, mediumblue = { 0, 0, 205, 1 }, darkblue = { 0, 0, 139, 1 }, navy = { 0, 0, 128, 1 }, midnightblue = { 25, 25, 112, 1 }, cornsilk = { 255, 248, 220, 1 }, blanchedalmond = { 255, 235, 205, 1 }, bisque = { 255, 228, 196, 1 }, navajowhite = { 255, 222, 173, 1 }, wheat = { 245, 222, 179, 1 }, burlywood = { 222, 184, 135, 1 }, tan = { 210, 180, 140, 1 }, rosybrown = { 188, 143, 143, 1 }, sandybrown = { 244, 164, 96, 1 }, goldenrod = { 218, 165, 32, 1 }, darkgoldenrod = { 184, 134, 11, 1 }, peru = { 205, 133, 63, 1 }, chocolate = { 210, 105, 30, 1 }, saddlebrown = { 139, 69, 19, 1 }, sienna = { 160, 82, 45, 1 }, brown = { 165, 42, 42, 1 }, maroon = { 128, 0, 0, 1 }, white = { 255, 255, 255, 1 }, snow = { 255, 250, 250, 1 }, honeydew = { 240, 255, 240, 1 }, mintcream = { 245, 255, 250, 1 }, azure = { 240, 255, 255, 1 }, aliceblue = { 240, 248, 255, 1 }, ghostwhite = { 248, 248, 255, 1 }, whitesmoke = { 245, 245, 245, 1 }, seashell = { 255, 245, 238, 1 }, beige = { 245, 245, 220, 1 }, oldlace = { 253, 245, 230, 1 }, floralwhite = { 255, 250, 240, 1 }, ivory = { 255, 255, 240, 1 }, antiquewhite = { 250, 235, 215, 1 }, linen = { 250, 240, 230, 1 }, lavenderblush = { 255, 240, 245, 1 }, mistyrose = { 255, 228, 225, 1 }, gainsboro = { 220, 220, 220, 1 }, lightgray = { 211, 211, 211, 1 }, silver = { 192, 192, 192, 1 }, darkgray = { 169, 169, 169, 1 }, gray = { 128, 128, 128, 1 }, dimgray = { 105, 105, 105, 1 }, lightslategray = { 119, 136, 153, 1 }, slategray = { 112, 128, 144, 1 }, darkslategray = { 47, 79, 79, 1 }, black = { 0, 0, 0, 1 } }
+-- TODO Combine this with Color class
+
 ColorUtils = {}
 
 ColorUtils.theme_yellow = { 0.83, 1, 0, 1 }
@@ -826,6 +1021,10 @@ function Vec2:squared_magnitude()
     return self.x * self.x + self.y * self.y
 end
 
+function Vec2:to_xy_pair()
+    return { self.x, self.y }
+end
+
 function Vec2.docs()
     local docstring = [[
 -- Vec2 Class Documentation --
@@ -1254,6 +1453,8 @@ function Point.new(vec2, options)
     local c = self.o.color or Color.new()
     self.color = Color.assign_color(c)
 
+    self.gradient = self.o.gradient or nil
+
     return self
 end
 
@@ -1285,19 +1486,21 @@ function Point:draw_coords()
 end
 
 function Point:draw_normal()
+    local paint = Paint.create(self.color, self.gradient)
     fill_circle(
         { self.vec2.x, self.vec2.y },
         self.radius,
-        color_paint(self.color:table())
+        paint
     )
 end
 
 function Point:draw_stroke()
+    local paint = Paint.create(self.color, self.gradient)
     stroke_circle(
         { self.vec2.x, self.vec2.y },
         self.radius,
         self.stroke_width,
-        color_paint(self.color:table()))
+        paint)
 end
 
 function Point:draw_char()
@@ -2171,6 +2374,36 @@ function LineGroup:print(places)
     print("  char_size: " .. char_size)
     print("  space_length: " .. space_length)
     print("")
+end
+Text = {}
+Text.__index = Text
+
+Text.id = 1
+
+function Text.new(string, options)
+    local self = setmetatable({}, Text)
+    self.element_id = Element.id
+    Element.id = Element.id + 1
+    self.class_id = Text.id
+    Text.id = Text.id + 1
+
+
+    self.string = string or ""
+    self.o = options or {}
+
+    self.vec2 = self.o.vec2 or Vec2.new()
+    self.size = self.o.size or 12
+    self.color = self.o.color or Color.new(theme.text)
+    return self
+end
+
+function Text:draw()
+    local scale_factor = self.size / 12
+    save()
+    translate(self.vec2:to_xy_pair())
+    scale { scale_factor, scale_factor }
+    text(self.string, self.color:table())
+    restore()
 end
 function button(x, y, width, options)
     -- Error Handling
