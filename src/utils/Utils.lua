ed82a018bbae3cd90ccf8cdecdab47292d68eab7
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-01-20 13:13:20 +0100

    Update Utils.lua
    Delete unnecessary utility function - this will now be done within classes that need it to be a little more legible.

--- src/utils/Utils.lua
+++ src/utils/Utils.lua
@@ -2,23 +2,6 @@
 
 Utils = {}
 
-function Utils.process_args(class_meta, ...)
-    local args = { ... }
-    local processed_args
-
-    if type(args[1]) == "table" then
-        if getmetatable(args[1]) == class_meta then
-            processed_args = args
-        else
-            processed_args = args[1]
-        end
-    else
-        processed_args = args
-    end
-
-    return processed_args
-end
-
 function Utils.has_non_integer_keys(t)
     for k, _ in pairs(t) do
         if type(k) ~= "number" or k ~= math.floor(k) or k < 1 then
