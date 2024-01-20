d6921676b82a9ccd545d382686469873d658cdfd
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-01-20 05:54:38 +0100

    Update Gradient.lua
    Gradient addition adds the two colors to one another

--- src/color/Gradient.lua
+++ src/color/Gradient.lua
@@ -1,3 +1,5 @@
+-- TODO implement operator overloading
+
 Gradient = {}
 G = Gradient
 Gradient.__index = Gradient
@@ -16,6 +18,20 @@ function Gradient.new(vec2a, vec2b, color1, color2)
     self.color1 = color1 or Color.new()
     self.color2 = color2 or Color.new()
     return self
+end
+
+function Gradient.__add(self, other)
+    return self:clone():add(other)
+end
+
+function Gradient:add(gradient)
+    local color1 = self.color1:add(gradient.color1)
+    local color2 = self.color2:add(gradient.color2)
+    return Gradient.new(self.vec2a, self.vec2b, color1, color2)
+end
+
+function Gradient:clone()
+    return Factory.clone_one(Gradient, self)
 end
 
 function Gradient:to_paint()
