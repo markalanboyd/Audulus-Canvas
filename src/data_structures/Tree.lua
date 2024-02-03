8ebb30e623e3f8fb355e36a6874b9d3f9ce2b3fe
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2024-02-03 13:09:08 +0100

    Update Tree.lua
    - Refactor dfs to be pre and post
    - Add bfs
    - Add add_structure, which adds a whole tree at once
    - Add call metamethod
    - Refactor tostring

--- src/data_structures/Tree.lua
+++ src/data_structures/Tree.lua
@@ -11,23 +11,28 @@ function Tree.new(value)
     return self
 end
 
--- TODO Print the tree out left to right
-function Tree:__tostring()
-    local function recursive_to_string(node, depth)
-        depth = depth or 0
-        local indent = string.rep("  ", depth)
-        local str = "TreeNode: val = " .. tostring(node.val)
+function Tree:__call(...)
+    local nodes = { ... }
 
-        if node.parent then
-            str = str .. "\n" .. indent .. "  ⮑  parent = " .. recursive_to_string(node.parent, depth + 1)
-        else
-            str = str .. "\n" .. indent .. "  ⮑  parent = nil"
+    for _, node in ipairs(nodes) do
+        self:add_child(node)
+    end
+end
+
+function Tree:__tostring()
+    local function tostring_r(node, depth, childNumber)
+        local indent = (depth > 0) and string.rep("    ", depth - 1) .. "    |- " or ""
+        local prefix = (depth > 0) and ("Child" .. childNumber .. ": ") or "Root: "
+        local str = indent .. prefix .. "val = " .. tostring(node.val)
+
+        for i, child in ipairs(node.children) do
+            str = str .. "\n" .. tostring_r(child, depth + 1, i)
         end
 
         return str
     end
 
-    return recursive_to_string(self)
+    return tostring_r(self, 0, 0)
 end
 
 function Tree:add_child(node)
@@ -36,6 +41,7 @@ function Tree:add_child(node)
     return self
 end
 
+-- TODO Should this return self or the child or both?
 function Tree:remove_child(node)
     local index
     for i, v in ipairs(self.children) do
@@ -49,15 +55,57 @@ function Tree:remove_child(node)
     return self
 end
 
-function Tree:dfs(t)
-    if self == nil then return end
+function Tree:dfs_pre(t)
     if not t then t = {} end
 
     table.insert(t, self)
 
     for _, node in ipairs(self.children) do
-        node:dfs(t)
+        node:dfs_pre(t)
     end
 
     return t
+end
+
+function Tree:dfs_post(t)
+    if not t then t = {} end
+
+    for _, node in ipairs(self.children) do
+        node:dfs_post(t)
+    end
+
+    table.insert(t, self)
+
+    return t
+end
+
+function Tree:bfs()
+    local function dequeue(t)
+        return table.remove(t, 1)
+    end
+    local visited = {}
+    local to_visit = {}
+    table.insert(to_visit, self)
+    while #to_visit ~= 0 do
+        local node = dequeue(to_visit)
+        table.insert(visited, node)
+        if node.children then
+            for _, child in ipairs(node.children) do
+                table.insert(to_visit, child)
+            end
+        end
+    end
+    return visited
+end
+
+function Tree:add_structure(tree_structure)
+    for _, v in ipairs(tree_structure) do
+        local node = Tree.new(v.val)
+        self:add_child(node)
+        if v.children then
+            node:add_structure(v.children)
+        end
+    end
+
+    return self
 end
